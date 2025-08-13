aa0aac7b41f811eeece33a83b4a2aed70cb5b032
Maxim Efremov <maxefremov04092004@gmail.com>
2025-08-13 17:01:25 +0300

    feat(layout): Сделал лэйаут

--- libs/root/shell/src/lib/sidepanel/sidepanel.ts
+++ libs/root/shell/src/lib/sidepanel/sidepanel.ts
@@ -1,11 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ChangeDetectionStrategy, Component, Type } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+type MenuCategory = {
+  icon: string;
+  name: string;
+  path: string;
+  component?: Type<any>;
+};
 
 @Component({
   selector: 'lib-sidepanel',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidepanel.html',
   styleUrl: './sidepanel.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Sidepanel {}
+export class Sidepanel {
+  public menu: MenuCategory[] = [
+    {
+      icon: 'assets/imgs/home.png',
+      name: 'Моя страница',
+      path: 'home',
+    },
+    {
+      icon: 'assets/imgs/chat.png',
+      name: 'Чаты',
+      path: 'chat',
+    },
+    {
+      icon: 'assets/imgs/lupa.png',
+      name: 'Поиск',
+      path: 'search',
+    },
+  ];
+}
